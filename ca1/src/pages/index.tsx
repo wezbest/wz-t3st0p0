f8f570de7e86e560ee89d91a04c488dab170d90c
wezbest <110793713+wezbest@users.noreply.github.com>
2023-05-17 17:56:29 +0000

    🎇

--- ca1/src/pages/index.tsx
+++ ca1/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, SignUp } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
@@ -16,7 +16,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          <h1>Signup Niggaz </h1>
+        </div>
+        <SignUp />
       </main>
     </>
   );
