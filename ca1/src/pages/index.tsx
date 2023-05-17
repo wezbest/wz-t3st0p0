699b4a3aefd167b80e22e67dd7dff0e5cbcbc8ad
wezbest <110793713+wezbest@users.noreply.github.com>
2023-05-17 20:43:17 +0000

    🎇

--- ca1/src/pages/index.tsx
+++ ca1/src/pages/index.tsx
@@ -14,7 +14,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  
+  const { data } = api.example.getAll.useQuery;
 
   return (
     <>
