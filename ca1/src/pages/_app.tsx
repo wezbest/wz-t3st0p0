e2f1ffdd3f9aae50f6298413fc92190b76428cf7
wezbest <110793713+wezbest@users.noreply.github.com>
2023-05-17 13:00:06 +0000

    🎇

--- ca1/src/pages/_app.tsx
+++ ca1/src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import { type AppType } from "next/app";
-
+import { type AppType, AppProps } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs/dist/components.server";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
